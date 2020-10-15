9db2c2896e773d4859c7e6a1a48166754f556c19
Alex Brufsky <alex@brufsky.org>
2020-10-15 19:28:06 -0400

    added test example and parsing examples, needs to be tested and tests written

--- functions/main_test.py
+++ functions/main_test.py
@@ -2,18 +2,26 @@ from unittest.mock import Mock
 
 import main
 
-def test_print_name():
+def test_todo_intent():
+    req_spoof =  {
+        'detect_intent_response_id': 'string',
+        'intent': {
+            'intent_id': 'string',
+            'params': {}
+        },
+        'tag': 'todo', # use this to determine action to take
+        'session': {
+            'session_id': 'string',
+            'params': {}
+        }
+    }
+    assert main.process_todo(req_spoof) == 'Nothing new!'
+
+#TODO
+def test_backend_activate():
     name = 'test'
-    data = {'name': name}
+    data = {'name': name} # replace with request
     req = Mock(get_json=Mock(return_value=data), args=data)
 
     # Call tested function
-    assert main.hello_http(req) == 'Hello {}!'.format(name)
-
-
-def test_print_hello_world():
-    data = {}
-    req = Mock(get_json=Mock(return_value=data), args=data)
-
-    # Call tested function
-    assert main.hello_http(req) == 'Hello World!'
+    assert main.backend_activate(req) == 'Hello!' ## replace with good test
