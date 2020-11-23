2f02754a49dbcc8879845118f8cab4131489ae46
Lance Bassett <lanbas@umich.edu>
2020-11-22 21:43:37 -0500

    Added file functionality. Need to add confirmation and sending.

--- functions/api.py
+++ functions/api.py
@@ -1,6 +1,8 @@
 # this module should 
 from canvasapi import Canvas
 from datetime import datetime, timedelta
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
 
 API_URL="https://umich.instructure.com"
 
@@ -72,3 +74,49 @@ class CanvasAPI:
       grades.append(gradeObj)
 
     return grades
+
+  def get_closest_files(self,
+                        file_name,
+                        class_name):
+    '''
+    Gets closest matches to given file_name for files in class with name class_name
+      file_name: string, name of desired file
+      class_name: string, name of class with file
+    Returns list of file objects
+    '''
+    # Find course the user is asking about
+    courses = self.canvas.get_user('self').get_courses(enrollment_state='active')
+    class_scores = []
+    for c in courses:
+      score = fuzz.token_set_ratio(class_name.lower(), str(c.course_code.lower()))
+      class_scores.append(score)
+
+    # Returns the index of *one of* of the maximum values, but I guess we have no way to break ties
+    course = courses[class_scores.index(max(class_scores))]
+
+    # Calculate token_set_ratio scores for all file names
+    class_files = list(course.get_files())
+    if len(class_files) == 0:
+      return "", 0
+
+    scores = []
+    for class_file in class_files:
+      score = fuzz.token_set_ratio(file_name.lower(), str(class_file).lower())
+      scores.append(score)
+
+    # Find and return top 3 guesses
+    best_files = []
+    bound = min(3, len(scores))
+    for i in range(bound):
+      max_idx = scores.index(max(scores))
+      best_files.append(class_files[max_idx])
+      scores.pop(max_idx)
+      class_files.pop(max_idx)
+
+    return best_files, course.id
+
+  
+
+  
+
+  
